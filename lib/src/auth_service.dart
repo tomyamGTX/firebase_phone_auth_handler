b1dd07100ac7e75d6615fe03403cd80290e58e5e
Hakimi Mdnoor <74492300+tomyamGTX@users.noreply.github.com>
2022-03-26 13:00:37 +0800

    Update auth_service.dart

--- lib/src/auth_service.dart
+++ lib/src/auth_service.dart
@@ -206,9 +206,10 @@ class FirebasePhoneAuthService extends ChangeNotifier {
 
     // Not on web.
     try {
-      _auth ??= FirebaseAuth.instance;
-      final authResult = await _auth!.signInWithCredential(authCredential!);
-      if (_onLoginSuccess != null) _onLoginSuccess!(authResult, autoVerified);
+//       _auth ??= FirebaseAuth.instance;
+//       final authResult = await _auth!.signInWithCredential(authCredential!);
+//       if (_onLoginSuccess != null) _onLoginSuccess!(authResult, autoVerified);
+      _onLoginSuccess!(authCredential!, autoVerified);
       return true;
     } on FirebaseAuthException catch (e) {
       if (_onLoginFailed != null) _onLoginFailed!(e);
